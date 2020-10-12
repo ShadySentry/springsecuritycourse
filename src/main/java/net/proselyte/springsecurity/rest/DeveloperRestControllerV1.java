de1e0f640d8a38f1fc179c719624f6cd58a33ba8
ShadySentry <eurohawk@gmail.com>
2020-10-12 15:21:18 +0300

    added access by authority

--- src/main/java/net/proselyte/springsecurity/rest/DeveloperRestControllerV1.java
+++ src/main/java/net/proselyte/springsecurity/rest/DeveloperRestControllerV1.java
@@ -1,6 +1,7 @@
 package net.proselyte.springsecurity.rest;
 
 import net.proselyte.springsecurity.model.Developer;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,11 +17,13 @@ public class DeveloperRestControllerV1 {
             new Developer(3L, "Sergey", "Sergeev")
     ).collect(Collectors.toList());
 
+    @PreAuthorize("hasAuthority('developers:read')")
     @GetMapping
     public List<Developer> getAll() {
         return DEVELOPERS;
     }
 
+    @PreAuthorize("hasAuthority('developers:read')")
     @GetMapping("/{id}")
     public Developer getById(@PathVariable Long id) {
         return DEVELOPERS.stream()
@@ -29,12 +32,14 @@ public class DeveloperRestControllerV1 {
                 .orElse(null);
     }
 
+    @PreAuthorize("hasAnyAuthority('developers:write')")
     @PostMapping
     public Developer create(@RequestBody Developer developer) {
         this.DEVELOPERS.add(developer);
         return developer;
     }
 
+    @PreAuthorize("hasAuthority('developers:write')")
     @DeleteMapping("/{id}")
     public void deleteById(@PathVariable Long id) {
         this.DEVELOPERS.removeIf(developer -> developer.getId().equals(id));
