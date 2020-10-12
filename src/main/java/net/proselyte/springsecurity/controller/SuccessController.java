aca7a5ddbc9ee1e5f0a79589d6b4bce3d5b0ad3f
ShadySentry <eurohawk@gmail.com>
2020-10-12 20:41:20 +0300

    secadded

--- src/main/java/net/proselyte/springsecurity/controller/SuccessController.java
+++ src/main/java/net/proselyte/springsecurity/controller/SuccessController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class SuccessController {
-    @GetMapping("/success")
+    @GetMapping("/auth/success")
     public String getSuccessPage(){
         return "success";
     }
