85ac344b71de111ac0ed7b1504db9b419dba56ff
ShadySentry <eurohawk@gmail.com>
2020-10-12 16:15:18 +0300

    added access by authority

--- src/main/java/net/proselyte/springsecurity/config/SecurityConfig.java
+++ src/main/java/net/proselyte/springsecurity/config/SecurityConfig.java
@@ -26,7 +26,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest()
                 .authenticated()
                 .and()
-                .formLogin();
+                .formLogin()
+                .loginPage("/auth/login").permitAll()
+                .defaultSuccessUrl("/auth/success");
     }
 
     @Bean
